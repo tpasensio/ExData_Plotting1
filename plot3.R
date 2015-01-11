41297a98029a62abfe8ff63aa7b3b449b13b8cce
jhernan <tpasensio@gmail.com>
2015-01-11 20:56:19 +0100

    results

--- plot3.R
+++ plot3.R
@@ -1,4 +1,4 @@
-setwd("/home/jhernan/Desktop/tanya/exploratory_data_analysis/ExData_Plotting1")
+#setwd("/home/jhernan/Desktop/tanya/exploratory_data_analysis/ExData_Plotting1")
 table <- read.csv("household_power_consumption.txt", header=TRUE,sep=";", colClasses=c("character","character","numeric", "numeric", "numeric","numeric", "numeric", "numeric", "numeric"), na.string="?")
 DateTime<-paste(table$Date, table$Time)
 table$DateTime<-strptime(DateTime,"%d/%m/%Y %H:%M:%S")
